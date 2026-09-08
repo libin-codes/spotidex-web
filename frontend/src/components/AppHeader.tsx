29798fb71e5bf08e8a2a24a0a2ce443ff1645434
libin-codes <libinlalu000@gmail.com>
2026-09-08 11:58:51 +0530

    feat: update AppHeader component styles and add GitHub link button;

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -3,13 +3,20 @@ import { FaGithub } from "react-icons/fa";
 
 export function AppHeader() {
   return (
-    <header className="flex h-12 shrink-0 items-center gap-2 border-b">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <h1 className="text-lg font-semibold font-mono">Spotidex
-        </h1>
+    <header className="flex h-14 shrink-0 items-center gap-2 border-b-2">
+      <div className="flex w-full items-center gap-2 px-4 lg:gap-2 lg:px-6">
+      
+        <h1 className="font-bold font-mono">Spotidex</h1> 
+      
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" size={"icon-lg"}  >
-            <FaGithub/>
+      
+          <Button variant="ghost" size={"icon-lg"} onClick={
+            ()=>{
+              window.location.href =
+                "https://github.com/libin-codes/spotidex-web";
+            }
+          }>
+            <FaGithub />
           </Button>
         </div>
       </div>
